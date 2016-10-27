da1311ad617e4f6660b08efd6022e5c57be2a686
Ryan Chavez <ryan.chavez@infusionsoft.com>
2016-10-27 13:12:26 -0700

    More stuff

--- app/src/main/java/com/infusionsoft/beerhub/BeerHubApplication.java
+++ app/src/main/java/com/infusionsoft/beerhub/BeerHubApplication.java
@@ -1,6 +1,8 @@
 package com.infusionsoft.beerhub;
 
 import android.app.Application;
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
 
 /**
  * File description here...
@@ -10,6 +12,15 @@ public class BeerHubApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+
+
+        /*RealmConfiguration config = new RealmConfiguration.Builder(this)
+            .name("beer-hub.realm")
+            .schemaVersion(0)
+            .deleteRealmIfMigrationNeeded()
+            .build();
+        Realm.setDefaultConfiguration(config);*/
     }
 
 }
--- app/src/main/java/com/infusionsoft/beerhub/MainActivity.java
+++ app/src/main/java/com/infusionsoft/beerhub/MainActivity.java
@@ -1,7 +1,9 @@
 package com.infusionsoft.beerhub;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void clickMe(View view) {
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
     }
 }

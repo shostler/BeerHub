9cf3d47d613ba52865a05d45cb2cd6bc1cdbbd48
Ryan Chavez <ryan.chavez@infusionsoft.com>
2016-10-27 12:47:31 -0700

    Fixed Kotlin issue and added Application class

---
+++ app/src/main/java/com/infusionsoft/beerhub/BeerHubApplication.java
@@ -0,0 +1,15 @@
+package com.infusionsoft.beerhub;
+
+import android.app.Application;
+
+/**
+ * File description here...
+ */
+public class BeerHubApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}

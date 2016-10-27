a62598645595b3c143c663b371df7f97f958369a
Ryan Chavez <ryan.chavez@infusionsoft.com>
2016-10-27 14:38:58 -0700

    Added default users and pin entry dep

--- app/src/main/java/com/infusionsoft/beerhub/PersonalDashboardActivity.java
+++ app/src/main/java/com/infusionsoft/beerhub/PersonalDashboardActivity.java
@@ -1,16 +1,42 @@
 package com.infusionsoft.beerhub;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import com.infusionsoft.beerhub.model.BeerDrinker;
+import com.infusionsoft.beerhub.model.BeerDrinkerFields;
+import io.realm.Realm;
 
 public class PersonalDashboardActivity extends AppCompatActivity {
+
+    public static final String PIN_KEY = "key.pin";
+
+    BeerDrinker drinker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_dashboard);
         //TODO show current totals on startup?
+
+        String pin = getIntent().getStringExtra(PIN_KEY);
+        Log.d("PersonalDashboard", pin);
+        Realm realm = Realm.getDefaultInstance();
+        drinker = realm.where(BeerDrinker.class)
+            .equalTo(BeerDrinkerFields.PIN, pin)
+            .findFirst();
+
+        realm.beginTransaction();
+        drinker.setBeersAdded(373);
+        realm.commitTransaction();
+
+        realm.executeTransaction(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                drinker.setBeersAdded(373);
+            }
+        });
     }
 
     public void clickAddCase(View view) {

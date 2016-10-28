bdbcaebeafe4a23d6d514beda2bd607431f88c35
Stuart Hostler <Stuart.Hostler@infusionsoft.com>
2016-10-28 10:44:51 -0700

    Add list of achievements below names on leaderboard

--- app/src/main/java/com/infusionsoft/beerhub/BeerDrinkerViewModel.java
+++ app/src/main/java/com/infusionsoft/beerhub/BeerDrinkerViewModel.java
@@ -1,7 +1,12 @@
 package com.infusionsoft.beerhub;
 
 import android.databinding.BaseObservable;
+
+import com.infusionsoft.beerhub.model.Achievement;
 import com.infusionsoft.beerhub.model.BeerDrinker;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * File description here...
@@ -19,4 +24,20 @@ public class BeerDrinkerViewModel extends BaseObservable {
         String nickName = drinker != null ? drinker.getNickName() : "";
         return nickName;
     }
+
+    public String getAchievements() {
+        List<String> achievementList = drinker != null ? drinker.getAchievements(): new ArrayList<String>();
+        String achievementsEarned;
+
+        StringBuilder sb = new StringBuilder();
+        for (String s : achievementList) {
+            sb.append(", ").append(Achievement.valueOf(s).getName());
+        }
+
+        if (sb.length() > 0) {
+            return sb.substring(2); // remove leading separator
+        }
+        return "";
+    }
+
 }
